6186c7a639e415bf72573ec50c3c24ef83376005
Humayun Ahmed Ashik <humayun_ruetcse13@yahoo.com>
2025-07-26 11:06:48 +0600

    experimenting github actions

---
+++ test/script.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const { formatDate } = require('../script.js');
+describe('Application Tests', () => {
+    it('hello world!', () => {
+        assert.strictEqual(1 + 1, 2);
+    });
+});
+
